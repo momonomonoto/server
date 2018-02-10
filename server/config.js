43f1d16d4e9a3525a507ca31f19bf701dcdda021
agapovalexbest <agapovalexbest@gmail.com>
2018-02-10 18:48:24 +0300

    Refactoring

--- server/config.js
+++ server/config.js
@@ -4,5 +4,8 @@ module.exports = {
   path: {
     view: path.resolve(__dirname, 'views'),
     static: path.resolve(__dirname, 'public')
-  }
+  },
+  mongodbUri: {
+      local: 'mongodb://localhost:27017/shop',
+  },
 };
--- server/services/db.js
+++ server/services/db.js
@@ -1,6 +1,6 @@
 const { MongoClient, ObjectID } = require('mongodb');
 const mongoose = require('mongoose');
-
+const config = require('../config');
 
 mongoose.Promise = global.Promise;
 
@@ -8,7 +8,7 @@ mongoose.connection.on('error', console.error.bind(console, 'connection error:')
 mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
 mongoose.connection.on('disconnected', () => console.log('Disconnected from MongoDB'));
 
-const db = mongoose.connect('mongodb://localhost:27017/shop');
+const db = mongoose.connect(config.mongodbUri.local);
 process.on('SIGINT', () => {
   mongoose.connection.close(() => {
     console.log('Mongoose disconnected through app termination');
