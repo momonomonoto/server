3afa77bb26788a06a25d356f3c9fda27b72b9a30
agapovalexbest <agapovalexbest@gmail.com>
2018-02-23 22:02:31 +0300

    Add mlab

--- server/config.js
+++ server/config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    sessionSecret: 'HacJmB3ma6crKKtK',
-    path: {
+  sessionSecret: 'HacJmB3ma6crKKtK',
+  path: {
     view: path.resolve(__dirname, 'views'),
     static: path.resolve(__dirname, 'public')
   },
   mongodbUri: {
-      local: 'mongodb://localhost:27017/shop',
-  },
+    local: 'mongodb://localhost:27017/shop',
+    mlab: 'mongodb://user:user@ds239648.mlab.com:39648/tes'
+  }
 };
