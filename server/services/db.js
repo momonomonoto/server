a0a6f0ea817bb17e13142b7cd5f52050c3f8d99c
agapovalexbest <agapovalexbest@gmail.com>
2018-03-03 19:47:46 +0300

    change local to mlab

--- server/services/db.js
+++ server/services/db.js
@@ -8,7 +8,7 @@ mongoose.connection.on('error', console.error.bind(console, 'connection error:')
 mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
 mongoose.connection.on('disconnected', () => console.log('Disconnected from MongoDB'));
 mongoose.debug = true;
-const db = mongoose.connect(config.mongodbUri.local);
+const db = mongoose.connect(config.mongodbUri.mlab);
 process.on('SIGINT', () => {
   mongoose.connection.close(() => {
     console.log('Mongoose disconnected through app termination');
