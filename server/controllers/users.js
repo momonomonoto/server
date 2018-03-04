85db2aad60d79ea9ac7230b070d0a97e4e746a95
agapovalexbest <agapovalexbest@gmail.com>
2018-03-04 21:13:32 +0300

    Hotfix. Show only users names.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,7 +3,7 @@ const modelUser = require('../models/users');
 module.exports = {
   showUsers: (param) => (req, res) => {
     const { apiRequest } = param || false;
-    modelUser.find().then((users) => {
+    modelUser.find({},{ name: 1, _id:0 }).then((users) => {
       if (apiRequest) return res.json(users);
       throw new Error();
     }).catch(err => res.status(500));
