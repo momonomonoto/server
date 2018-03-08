2dba20c05220e5702ef75573dbc885aaeedb9414
agapovalexbest <agapovalexbest@gmail.com>
2018-03-08 19:52:15 +0300

    Add author to commentary

--- server/controllers/commentary.js
+++ server/controllers/commentary.js
@@ -7,10 +7,11 @@ module.exports = {
   createCommentary(req, res) {
     const { title, text } = req.body;
     const { id } = req.params;
+    const author = req.user.name;
     const description = text;
       modelProjects.findOne({ id }, (err, elem) => {
         if (Boolean(id) === false) throw new Error('Id is not valid');
-        const commentaries = [].concat(elem.commentaries,[{title,description}]);
+        const commentaries = [].concat(elem.commentaries,[{title,description,author}]);
         modelProjects.update({id}, {commentaries}, () => {res.redirect(`/project/${id}`);})
       });
 
